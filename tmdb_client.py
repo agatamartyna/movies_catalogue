f661e0d8b31cfe292af7d0c19d3abf2b01e65fae
Agata Martyna <agatamartynachalupka@gmail.com>
2020-12-31 12:31:09 +0100

    Add function

--- tmdb_client.py
+++ tmdb_client.py
@@ -2,14 +2,19 @@ import requests
 
 API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNWU0NDU3ODA0ZmFiODZkMjdkYTViM2RjOTE2OGJkNiIsInN1YiI6IjVmZDRmOTk1MmNlZmMyMDAzZTFkYWUxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.onqQhBrrSb7-8x75xIczdx0CGk5_rmEmvL2Lx9PMnac"
 
-def get_movies_list(list_type='popular'):
-    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+def make_request(endpoint):
     headers = {
         "Authorization": f"Bearer {API_TOKEN} "
     }
     response = requests.get(endpoint, headers=headers)
+    return response
+
+
+def get_movies_list(list_type='popular'):
+    response = make_request(f"https://api.themoviedb.org/3/movie/{list_type}")
     response.raise_for_status()
     return response.json()
+
 
 def get_movies(how_many, list_type='popular'):
     data = get_movies_list(list_type)
@@ -24,31 +29,16 @@ def get_image_url(path, size="w342"):
 
         
 def get_single_movie(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN} "
-    }
-    response = requests.get(endpoint, headers=headers)
-
+    response = make_request(f"https://api.themoviedb.org/3/movie/{movie_id}")
     return response.json()
 
 def get_single_movie_cast(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN} "
-    }
-    response = requests.get(endpoint, headers=headers)
-
+    response = make_request(f"https://api.themoviedb.org/3/movie/{movie_id}/credits")
     return response.json()['cast']
 
 
 def get_backdrop(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN} "
-    }
-    response = requests.get(endpoint, headers=headers)
-
+    response = make_request(f"https://api.themoviedb.org/3/movie/{movie_id}/images")
     return response.json()
 
 
