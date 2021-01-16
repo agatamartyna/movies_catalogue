5f8cbf89fd0bb2a257c89c5e28950aebbd6c9568
Agata Martyna <agatamartynachalupka@gmail.com>
2021-01-16 22:28:08 +0100

    fix for PEP

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from random import choice
 
 app = Flask(__name__)
 
+
 @app.context_processor
 def utility_processor():
     def tmbd_image_url(path, size):
         return tmdb_client.get_image_url(path, size)
     return {"tmdb_image_url": tmbd_image_url}
+
 
 @app.route('/')
 def homepage():
@@ -17,7 +19,8 @@ def homepage():
     if selected_list not in lists:
         selected_list = "popular"
     movies = tmdb_client.get_movies(how_many=8, list_type=selected_list, )
-    return render_template("homepage.html", movies=movies, current_list=selected_list, lists=lists)
+    return render_template("homepage.html", movies=movies,
+                           current_list=selected_list, lists=lists)
 
 
 @app.route("/movie/<movie_id>")
@@ -26,7 +29,8 @@ def movie_details(movie_id):
     cast = tmdb_client.get_single_movie_cast(movie_id)
     backdrop_images = tmdb_client.get_backdrop(movie_id)
     random_backdrop = choice(backdrop_images['backdrops'])
-    return render_template("movie_details.html", movie=details, cast=cast, random_backdrop=random_backdrop)
+    return render_template("movie_details.html", movie=details,
+                           cast=cast, random_backdrop=random_backdrop)
 
 
 if __name__ == "__main__":
