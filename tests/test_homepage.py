3c6613de006513e923eec5e9f9a6febb7200742a
Agata Martyna <agatamartynachalupka@gmail.com>
2021-01-18 20:17:54 +0100

    fix for PEP

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -2,14 +2,13 @@ from main import app
 from unittest.mock import Mock
 import pytest
 
+
 @pytest.mark.parametrize('endpoint, result', (
     ('popular', 200),
     ('upcoming', 200),
     ('top_rated', 200),
     ('now_playing', 200)
 ))
-
-
 def test_homepage(monkeypatch, endpoint, result):
     api_mock = Mock(return_value={'results': []})
     monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
